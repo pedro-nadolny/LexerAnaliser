c72d4b993406402a6a68a60a5075c1b3220bb21b
Pedro Roberto <pedro.nadolny@gmail.com>
2017-06-14 19:30:05 -0300

    O loco bixo.

---
+++ lexer.rb
@@ -0,0 +1,15 @@
+class Token   
+    Characters tipo
+    Number lexama
+end
+
+def scan(input = "") 
+    i = 0
+    l = Array .new
+
+    while i <= input.lenght 
+        t = Token .new
+        
+        
+    end
+end
